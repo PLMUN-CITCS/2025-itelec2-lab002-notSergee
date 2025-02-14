c66f0376c8575f5e38b0b379b80b55cb7a6eda10
notSergee <>
2025-02-14 19:15:01 +0800

    Last Sync: 2025-02-14 11:15:01 (Mobile)

--- problem_01.py
+++ problem_01.py
@@ -1,6 +1,17 @@
 num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
+
+sum_result = num1 + num2
+diff_result = num1 - num2
+prod_result = num1 * num2
+
+if num2 !=0:
+quotient_result = round(num1 / num2, 2)
+else:
+quotient_result = "undefined"
+
+
 print(f"The sum is {num1 + num2}")
 print(f"The difference is {num1 - num2}")
 print(f"The product is {num1 * num2}")
--- problem_02.py
+++ problem_02.py
@@ -1,24 +1,16 @@
-# Yusingco, Serge Oland
+# Yusingco, Serge Oland C
 # ITELEC2
-# Problem Set 01 - Problem 01
-# Simple Calculator Program
+# Problem Set 01 - Problem 02
+# Square the Number Program
 
 def main():
-    print("Simple Calculator Program")
+    print("Square the Number Program")  # Introduce the program
+
+    # Obtain user input
+    number = int(input("Enter a number: "))
+
+    # Compute and display the square
+    print(f"The square of {number} is {(number * number):.2f}")
 
 if __name__ == "__main__":
     main()
-    
-num1 = int(input("Enter 1st number: "))
-num2 = int(input("Enter 2nd number: "))
-
-print(f"The sum is {num1 + num2}")
-
-print(f"The difference is {num1 - num2}")
-
-print(f"The product is {num1 * num2}")
-
-print(f"The quotient is {(num1 / num2):.2f}")      
-
-  
-    
