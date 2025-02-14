c71138857f20ba17e7c19d1d845fcf3f388cad73
notSergee <>
2025-02-14 18:35:29 +0800

    Last Sync: 2025-02-14 10:35:29 (Mobile)

---
+++ python problem_01.py
@@ -0,0 +1,24 @@
+# Yusingco, Serge Oland
+# ITELEC2
+# Problem Set 01 - Problem 01
+# Simple Calculator Program
+
+def main():
+    print("Simple Calculator Program")
+
+if __name__ == "__main__":
+    main()
+    
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+print(f"The sum is {num1 + num2}")
+
+print(f"The difference is {num1 - num2}")
+
+print(f"The product is {num1 * num2}")
+
+print(f"The quotient is {(num1 / num2):.2f}")      
+
+  
+    
